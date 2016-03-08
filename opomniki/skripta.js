7c368ab895192966473fda22ec8afa1ee6cc7a21
Matic Oblak <mo2625@student.uni-lj.si>
2016-03-08 17:25:24 +0000

    dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -11,6 +11,23 @@ window.addEventListener('load', function() {
 
 	document.querySelector("#prijavniGumb").addEventListener('click', izvediPrijavo);
 	
+	
+	var dodajOpomnik=function(event){
+		
+		var naziv = document.querySelector("#naziv_opomnika").value;
+		var cas_opomnika = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value="";
+		document.querySelector("#cas_opomnika").value="";
+		
+		document.querySelector("#opomniki").innerHTML+= " <div class='opomnik'> <div class='naziv_opomnika'>" +naziv +" </div> <div class='cas_opomnika'> Opomnik čez <span>" + cas_opomnika+"</span> sekund.</div> </div>";
+	
+		
+	};
+	
+	
+	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik);
+	
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
